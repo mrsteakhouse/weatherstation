1a7529c66a3640278753cbe90fff8e65c1d88422
Fabian Frey <fabian.frey@ninjaneers.de>
2020-11-02 08:00:19 +0100

    fix(homie-spec): use floats

--- homie.sh
+++ homie.sh
@@ -12,21 +12,21 @@ mosquitto_pub -r -h 192.168.0.59 -t 'homie/living-room-temp/temperature/$propert
 mosquitto_pub -r -h 192.168.0.59 -t 'homie/living-room-temp/temperature/temp' -m '0'
 mosquitto_pub -r -h 192.168.0.59 -t 'homie/living-room-temp/temperature/temp/$name' -m 'Temperature'
 mosquitto_pub -r -h 192.168.0.59 -t 'homie/living-room-temp/temperature/temp/$unit' -m '°C'
-mosquitto_pub -r -h 192.168.0.59 -t 'homie/living-room-temp/temperature/temp/$datatype' -m 'integer'
+mosquitto_pub -r -h 192.168.0.59 -t 'homie/living-room-temp/temperature/temp/$datatype' -m 'float'
 mosquitto_pub -r -h 192.168.0.59 -t 'homie/living-room-temp/temperature/temp/$settable' -m 'false'
 
 # Humidity
 mosquitto_pub -r -h 192.168.0.59 -t 'homie/living-room-temp/temperature/hum' -m '0'
 mosquitto_pub -r -h 192.168.0.59 -t 'homie/living-room-temp/temperature/hum/$name' -m 'Humidity'
 mosquitto_pub -r -h 192.168.0.59 -t 'homie/living-room-temp/temperature/hum/$unit' -m '%'
-mosquitto_pub -r -h 192.168.0.59 -t 'homie/living-room-temp/temperature/hum/$datatype' -m 'integer'
+mosquitto_pub -r -h 192.168.0.59 -t 'homie/living-room-temp/temperature/hum/$datatype' -m 'float'
 mosquitto_pub -r -h 192.168.0.59 -t 'homie/living-room-temp/temperature/hum/$settable' -m 'false'
 
 # Pressure
 mosquitto_pub -r -h 192.168.0.59 -t 'homie/living-room-temp/temperature/p' -m '0'
 mosquitto_pub -r -h 192.168.0.59 -t 'homie/living-room-temp/temperature/p/$name' -m 'Air Pressure'
 mosquitto_pub -r -h 192.168.0.59 -t 'homie/living-room-temp/temperature/p/$unit' -m 'hPa'
-mosquitto_pub -r -h 192.168.0.59 -t 'homie/living-room-temp/temperature/p/$datatype' -m 'integer'
+mosquitto_pub -r -h 192.168.0.59 -t 'homie/living-room-temp/temperature/p/$datatype' -m 'float'
 mosquitto_pub -r -h 192.168.0.59 -t 'homie/living-room-temp/temperature/p/$settable' -m 'false'
 
 #  Pump pressure state
